3dbee787651cd92ed80de0641fc880f8732accfc
Tyffoni <tyffoni@gmail.com>
2026-02-14 15:40:22 -0800

    Co-authored-by: Rachel Tran <ra2y@users.noreply.github.com>

---
+++ app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:3001",
+        "http://127.0.0.1:3001",
+        "*"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def index():
+    return "index.html"
+
+@app.get("/api/advice")
+def get_advice():
+
+    return 
