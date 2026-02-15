e5d56760fe87d53f03d12c231198f6cd5c6691d1
Cindy Tat <cindyannetat@gmail.com>
2026-02-14 17:33:56 -0800

    testing pages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,7 +25,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Energy Dashboard',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
